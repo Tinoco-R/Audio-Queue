4b7d5824d0f7e91e70493aa11a38420013229ae3
Raphael Tinoco <raphaelt854@gmail.com>
2025-10-03 06:29:19 -0500

    fix: Vercel deployment

--- src/components/myPage.tsx
+++ src/components/myPage.tsx
@@ -1,12 +1,10 @@
 'use client';
 import React from "react";
-import { useRouter, usePathname, useSearchParams } from 'next/navigation'
-import LinkablePlatforms from "@/components/linkedAccounts"
+import { useRouter } from 'next/navigation'
 
 export default function MyPage() {
   const router = useRouter()
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
+  
     return(
         <div>
             <button type="button" onClick={() => router.push('/linkPlatforms')}>Link Platforms</button>
