fd3fc736f7dccea37bdd65a723edf59a689e6ec9
Raphael Tinoco <raphaelt854@gmail.com>
2025-10-07 17:56:31 -0500

    fix: Rendering issues
    Selectable platforms showing as black on refresh with no regard to linked platforms (fixed)

--- src/components/linkedAccounts.tsx
+++ src/components/linkedAccounts.tsx
@@ -4,10 +4,6 @@ import Platform from "./platform";
 
 // Returns platforms that the search will be able to use per user platform authorizations
 function getLinkedPlatforms(returnDisabled = false) {
-    if (typeof window === 'undefined') {
-        return [false, false, false, false];
-    }
-
     const keys = ["access_token_spotify", "access_token_youtube", "access_token_apple_music", "access_token_soundcloud"];
     const linked = [];
 
